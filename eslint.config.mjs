c37b7677208fb6fe0e3a3aea92cefd6ae67de98a
Chinenye <anikwenzekelly@gmail.com>
2025-12-23 09:47:07 +0100

    feat: update packages with next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,72 +1,46 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import globals from 'globals'
-import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
+import nextPlugin from '@next/eslint-plugin-next'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
+  // 1. Files to ignore
   {
-    ignores: [],
+    ignores: ['.next/**', 'node_modules/**', 'dist/**', '.contentlayer/**'],
   },
+
+  // 2. Base Javascript rules
   js.configs.recommended,
-  ...compat.extends(
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-    'next',
-    'next/core-web-vitals'
-  ),
+
+  // 3. Next.js and Typescript Configuration
   {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     plugins: {
-      '@typescript-eslint': typescriptEslint,
+      '@next/next': nextPlugin,
+      '@typescript-eslint': tsPlugin,
     },
-
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.amd,
-        ...globals.node,
-      },
-
       parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: 'commonjs',
-
       parserOptions: {
-        project: true,
-        tsconfigRootDir: __dirname,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
     },
-
     rules: {
-      'prettier/prettier': 'error',
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      ...tsPlugin.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
-
-      'jsx-a11y/anchor-is-valid': [
-        'error',
-        {
-          components: ['Link'],
-          specialLink: ['hrefLeft', 'hrefRight'],
-          aspects: ['invalidHref', 'preferButton'],
-        },
-      ],
+      '@next/next/no-img-element': 'off',
       'react/prop-types': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
       'react/no-unescaped-entities': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-require-imports': 'off',
       '@next/next/no-img-element': 'off',
+      'no-undef': 'off',
     },
   },
 ]
