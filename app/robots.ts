e7eb2135738bcafd4f90a8e969204a83b01a71a1
Chinenye <anikwenzekelly@gmail.com>
2026-01-20 10:19:38 +0100

    feat: update favicons robots file

--- app/robots.ts
+++ app/robots.ts
@@ -5,10 +5,16 @@ export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/', '/static/favicons/'], // Explicitly allow the favicon folder
+      },
+      {
+        userAgent: 'Googlebot-Image', // Specifically allow Google's image crawler
+        allow: '/static/favicons/',
+      },
+    ],
     sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
     host: siteMetadata.siteUrl,
   }
